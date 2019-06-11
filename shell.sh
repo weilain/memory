71e4a3aa705c68dc131fb0c81310e7f91e8d67d7
谌壮壮 <shenzhuang@aliyun.com>
2019-06-11 17:31:42 +0800

    shell

--- shell.sh
+++ shell.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 echo "Hello World !"
+
+#.............................
+cd ~
+mkdir shell_tut
+cd shell_tut
+
+for ((i=0; i<10; i++)); do
+	touch test_$i.txt
+done
+#..........................
